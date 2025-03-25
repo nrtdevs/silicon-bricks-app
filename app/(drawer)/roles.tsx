ffc9708f75d632718cb63f61441dd5fd6af10abd
Akashmajumdar88 <akashmajumdar88@gmail.com>
2025-03-25 11:11:06 +0530

    25

--- app/(drawer)/roles.tsx
+++ app/(drawer)/roles.tsx
@@ -1,14 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { ScaledSheet } from "react-native-size-matters";
 
 const Roles = () => {
   return (
     <View>
-      <Text>roles</Text>
+      <Text style={styles.text}>roles</Text>
     </View>
   );
 };
 
 export default Roles;
 
-const styles = StyleSheet.create({});
+const styles = ScaledSheet.create({
+  text:{
+    fontSize:"24@ms"
+  }
+});
--- graphql/Query.ts
+++ graphql/Query.ts
@@ -18,3 +18,17 @@ export const GetAllOrganisations = gql`
     }
   }
 `;
+
+export const GetAllProjects = gql`
+  query PaginatedProjects($listInputDto: ListInputDTO!) {
+  paginatedProjects(ListInputDTO: $listInputDto) {
+    data {
+      id
+      name
+      description
+      status
+      organizationId
+    }
+  }
+}
+`;
