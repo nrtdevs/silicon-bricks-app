180ee8173d0fd099036cc04e412ca9daef5808c5
erashok777 <168190203+erashok777@users.noreply.github.com>
2025-05-22 18:18:23 +0530

    vehicle breakdown

--- app/index.tsx
+++ app/index.tsx
@@ -29,9 +29,8 @@ const Index = () => {
 
   if (loading) return null;
 
-  if (!token) return <Redirect href="/(auth)/login" />;
-  return <Redirect href="/(auth)/login" />;
-  // return <Redirect href="/(subComponents)/dashboard" />;
+  if (!token) return <Redirect href="/(auth)/login" />; 
+  return <Redirect href="/(subComponents)/dashboard" />;
   // return <Redirect href="/(drawer)/(tabs)" />;
 };
 
